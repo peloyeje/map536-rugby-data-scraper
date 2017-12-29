a1f79e29c995fe67c9964f35c02ce1f2d4a9db19
aredier <antoine.redier2@gmail.com>
2017-12-29 15:24:42 +0100

    follow match links from match list page

--- rugby_scraper/rugby_scraper/spiders/rugby_spider.py
+++ rugby_scraper/rugby_scraper/spiders/rugby_spider.py
@@ -1,3 +1,6 @@
+import re
+import scrapy
+
 class rugby_spider (scrapy.Spider) :
     """main spider of the scraper that will get all the statistics from the different pages of the website http://stats.espnscrum.com"""
 
@@ -11,7 +14,7 @@ class rugby_spider (scrapy.Spider) :
         - ordered by date
         """
 
-        urls = ["http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=1;spanmin1=24+jul+1992;spanval1=span;template=results;type=team;view=match"]
+        urls = ["http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=1;spanmin1=15+aug+1992;spanval1=span;template=results;type=team;view=match"]
         for url in urls :
             yield scrapy.Request(url = url, callback = self.match_list_parse)
 
@@ -22,7 +25,20 @@ class rugby_spider (scrapy.Spider) :
         it also find the next list page to find
         """
 
-        pass
+        #selecting the matches menu to parse match info page
+        MENU_SELECTOR = ".engine-dd"
+        for menu in response.css(MENU_SELECTOR):
+            menu_id = menu.css("div::attr(id)").extract_first()
+            if re.search("^engine-dd[0-9]+$", menu_id) :
+
+                #selecting the match description link in the page
+                MATCH_LINK_SELECTOR = "ul li:nth-child(6) a::attr(href)"
+                match_link = menu.css(MATCH_LINK_SELECTOR).extract_first()
+                yield response.follow(match_link, callback = self.match_page_parse)
+
+
+
+
 
 
     def player_page_parse(self, response):
@@ -40,4 +56,4 @@ class rugby_spider (scrapy.Spider) :
         - team statistics format : {...}
         """
 
-        pass
+        pass 
