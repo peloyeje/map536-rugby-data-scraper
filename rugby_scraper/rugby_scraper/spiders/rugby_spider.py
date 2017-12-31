8df55405d67323869e83f186318bbe411653870f
aredier <antoine.redier2@gmail.com>
2017-12-31 18:35:12 +0100

    added match stats parsing

--- rugby_scraper/rugby_scraper/spiders/rugby_spider.py
+++ rugby_scraper/rugby_scraper/spiders/rugby_spider.py
@@ -15,7 +15,7 @@ class rugby_spider (scrapy.Spider) :
         - ordered by date
         """
 
-        urls = ["http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=1;spanmin1=15+aug+1992;spanval1=span;template=results;type=team;view=match"]
+        urls = ["http://stats.espnscrum.com/statsguru/rugby/stats/index.html?class=1;filter=advanced;orderby=date;page=150;spanmin1=15+aug+1992;spanval1=span;template=results;type=team;view=match"]
         for url in urls :
             yield scrapy.Request(url = url, callback = self.match_list_parse)
 
@@ -169,6 +169,147 @@ class rugby_spider (scrapy.Spider) :
             away_team_player_dic[player_id] = (player_name, player_position, player_number)
 
         return (home_team_player_dic, away_team_player_dic)
+
+    def _parse_match_stats(self, info, team) :
+        """method that parses the Match stats tab of the match data
+        format : {"match_id" : "placeholder", "team_id" : "placeholder", "pens_attempt" : int, "drops_attempt" : int, "kicks" : int, "passes" : int, "runs" : int, "meters" : int, "def_beaten" : int, "offloads" : int, "rucks_init" : init , "rucks_won" : int , "mall_init" : int, "mall_won" : int, "turnovers" : int, "tackles_made" : int, "tackles_missed" : int, "scrums_won_on_feed" : int, "scrums_lost_on_feed" : int, "lineouts_won_on_throw" : int, "lineouts_lost_on_throw" : int, }"""
+
+        match_stats = {"match_id" : "placeholder", "team_id" : "placeholder"}
+        assert team in ["home", "away"] , "team to analyse must be either home or away"
+        DATA_LINE_SELECTOR = "table tr"
+        if not info.css(DATA_LINE_SELECTOR) :
+            return None
+        for data_line in info.css(DATA_LINE_SELECTOR) :
+            #selecting data_line title
+            title = data_line.css("td:nth-child(2)::text")
+            if not title:
+                continue
+            title = title.extract_first()
+            #selecting data_line value
+            if team == "home" :
+                VALUE_SELECTOR = "td:nth-child(1)::text"
+            else:
+                VALUE_SELECTOR = "td:nth-child(3)::text"
+            value = data_line.css(VALUE_SELECTOR)
+            if not value:
+                continue
+            value = value.extract_first()
+
+            #analysing the data itself
+            #attempted penalties
+            if title == "Penalty goals":
+                cons_attempt_re = regex.match("[0-9]+ from ([0-9]+)", value)
+                if not cons_attempt_re :
+                    continue
+                cons_attempt = int(cons_attempt_re.captures(1)[0])
+                match_stats["pens_attempt"] = cons_attempt
+            #attempted drops
+            if title == "Dropped goals":
+                drops_re = regex.match("([0-9]+)( \(([0-9]+) missed\))?", value)
+                if not drops_re:
+                    continue
+                drops_scored = int(drops_re.captures(1)[0])
+                drops_missed = drops_re.captures(3)
+                if not drops_missed:
+                    drops_missed = 0
+                else:
+                    drops_missed = int(drops_missed[0])
+                drops_attempt = drops_scored + drops_missed
+                match_stats["drops_attempt"] = drops_attempt
+            #kicks from hand
+            if title == "Kicks from hand":
+                kicks = int(value)
+                match_stats["kicks"] = kicks
+            #passes
+            if title == "Passes":
+                passes = int(value)
+                match_stats["passes"] = passes
+            #runs
+            if title == "Runs":
+                runs = int(value)
+                match_stats["runs"] = runs
+            #meters run with ball
+            if title == "Metres run with ball":
+                meters = int(value)
+                match_stats["meters"] = meters
+            #clean breacks
+            if title == "Clean breaks":
+                breaks = int(value)
+                match_stats["breaks"] = breaks
+            #defenders beaten
+            if title == "Defenders beaten":
+                def_beaten = int(value)
+                match_stats["def_beaten"] = def_beaten
+            #Offloads
+            if title == "Offloads":
+                offloads = int(value)
+                match_stats["offloads"] = offloads
+            #rucks both initiated and won
+            if title == "Rucks won":
+                rucks_re = regex.match("^\\n([0-9]+) from ([0-9]+)", value)
+                if not rucks_re :
+                    continue
+                rucks_init = int(rucks_re.captures(2)[0])
+                match_stats["rucks_init"] = rucks_init
+                rucks_won = int(rucks_re.captures(1)[0])
+                match_stats["rucks_won"] = rucks_won
+            #mauls both initiated and won
+            if title == "Mauls won":
+                mall_re = regex.match("^\\n([0-9]+) from ([0-9]+)", value)
+                if not mall_re :
+                    continue
+                mall_init = int(mall_re.captures(2)[0])
+                match_stats["mall_init"] = mall_init
+                mall_won = int(mall_re.captures(1)[0])
+                match_stats["mall_won"] = mall_won
+            #turnovers
+            if title == "Turnovers conceded":
+                turnovers = int(value)
+                match_stats["turnovers"] = turnovers
+            #tackles
+            if title == "Tackles made/missed":
+                tackles_re = regex.match("^([0-9]+)/([0-9]+)$", value)
+                if not tackles_re :
+                    continue
+                tackles_made = int(tackles_re.captures(1)[0])
+                match_stats["tackles_made"] = tackles_made
+                tackles_missed = int(tackles_re.captures(2)[0])
+                match_stats["tackles_missed"] = tackles_missed
+            #scrums
+            if title == "Scrums on own feed":
+                scrums_re = regex.match("^\\n\\t  ([0-9]+) won, ([0-9]+) lost", value)
+                if not scrums_re:
+                    continue
+                scrums_won_on_feed = int(scrums_re.captures(1)[0])
+                match_stats["scrums_won_on_feed"] = scrums_won_on_feed
+                scrums_lost_on_feed = int(scrums_re.captures(2)[0])
+                match_stats["scrums_lost_on_feed"] = scrums_lost_on_feed
+            #lineouts
+            if title == "Lineouts on own throw":
+                lineout_re = regex.match("^\\n\\t  ([0-9]+) won, ([0-9]+) lost", value)
+                if not lineout_re:
+                    continue
+                lineouts_won_on_throw = int(lineout_re.captures(1)[0])
+                match_stats["lineouts_won_on_throw"] = lineouts_won_on_throw
+                lineouts_lost_on_throw = int(lineout_re.captures(2)[0])
+                match_stats["lineouts_lost_on_throw"] = lineouts_lost_on_throw
+            #penalties
+            if title == "Penalties conceded":
+                penalties = int(value)
+                match_stats["penalties"] = penalties
+            #cards
+            if title == "Yellow/red cards":
+                cards_re = regex.match("^([0-9]+)/([0-9]+)$", value)
+                if not cards_re:
+                    continue
+                yellow_cards = int(cards_re.captures(1)[0])
+                match_stats["yellow_cards"] = yellow_cards
+                red_cards = int(cards_re.captures(2)[0])
+                match_stats["red_cards"] = red_cards
+
+
+        return match_stats
+
 
     def _match_iframe_parse(self, response):
         """parser for the internal iframe of each match page"""
@@ -502,15 +643,28 @@ class rugby_spider (scrapy.Spider) :
                             else :
                                 away_team_score_data["drops"].append(player_id)
 
-                yield{"score_data" : home_team_score_data}
-                yield{"score_data" : away_team_score_data}
+                #yield{"score_data" : home_team_score_data}
+                #yield{"score_data" : away_team_score_data}
 
 
 
 
 
             elif title == "Match stats":
-                pass
+                home_match_stats = self._parse_match_stats(info, team = "home")
+                if not home_match_stats :
+                    continue
+                home_match_stats["match_id"] = match_id
+                home_match_stats["team_id"] = home_team_id
+                yield {"match_stat_data" : home_match_stats}
+
+                away_match_stats = self._parse_match_stats(info, team = "away")
+                if not away_match_stats:
+                    continue
+                away_match_stats["match_id"] = match_id
+                away_match_stats["team_id"] = away_team_id
+                yield {"match_stat_data" : away_match_stats}
+
             elif title == "Timeline":
                 pass
             elif re.search("^[a-zA-Z]+ stats$", title) :
