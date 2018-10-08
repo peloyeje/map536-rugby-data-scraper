80b09f7d370e7360828bc5dc151ab22ed951349b
Jean-Eudes Peloye <hello@peloye.je>
2018-10-08 14:19:00 +0200

    Remove helpers

--- homebaked_data_science_helpers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-package that provides base classes for PCA and clustering
-"""
-
-from .kmeans import KMeans
-from .wards import Wards
-from .pca import PCA, pca_imputation
-from .helpers import standardize
